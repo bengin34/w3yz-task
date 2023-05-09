81a7f78e8a9dc4b9934e46049a2c96914325ec18
bengin34 <becaglar3434@gmail.com>
2023-05-10 00:20:09 +0300

    Visit

--- src/components/Ziyaret.jsx
+++ src/components/Ziyaret.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { BsChevronCompactRight } from "react-icons/bs";
+
+import { visit } from "../helper/data";
 const Ziyaret = () => {
-
-    const ziyaretTipleri = [
-        {
-          icon: ,
-          text:true,
-
-        },
-      ];
-
   return (
     <div className="h-[638px] grid grid-rows-3 grid-cols-4 gap-4 m-[150px] ">
       <div class=" row-start-1 row-end-4 col-start-1 col-span-2  flex  flex-col justify-center items-center">
@@ -20,24 +12,21 @@ const Ziyaret = () => {
           bu muhteşem müzeyi nasıl keşfetmek istersin?
         </h1>
       </div>
-      <div class="col-start-3 col-end-3 flex justify-center items-center">
-        Aile ile ziyaret
-      </div>
-      <div class="col-start-4 col-end-4 flex justify-center items-center">
-        Grup olarak ziyaret
-      </div>
-      <div class="col-start-3 col-end-3 flex justify-center items-center">
-        Araştırma için ziyaret
-      </div>
-      <div class="col-start-4 col-end-4 flex justify-center items-center">
-        Engelinize göre ziyaret
-      </div>
-      <div class="col-start-3 col-end-3 flex justify-center items-center">
-        Kurumsal eğitim için ziyaret
-      </div>
-      <div class="col-start-4 col-end-4 flex justify-center items-center">
-        Sponsor olmak için ziyaret
-      </div>
+
+      {visit.map((item, index) => (
+        <div
+          className={`col-start-${
+            index % 2 === 0 ? 3 : 4
+          } col-end-${
+            index % 2 === 0 ? 3 : 4
+          } flex justify-center items-center`}
+          key={index}
+        >
+        <div className="flex flex-row justify-between items-center"> <img className="mr-4" src={item.img} alt="" />
+          {item.showText}</div>
+         
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/helper/data.js
@@ -0,0 +1,35 @@
+import disabled from '../assets/disabled.png'
+import family from '../assets/family.png'
+import group from '../assets/group.png'
+import sponsor from '../assets/sponsor.png'
+import official from '../assets/official.png'
+import research from '../assets/research.png'
+
+
+export const visit = [
+    {
+      img: family,
+      showText:'Aile ile ziyaret',
+    },
+    {
+      img: disabled,
+      showText:'Engelinize göre ziyaret',
+    },
+    {
+      img: research,
+      showText:'Araştırma için ziyaret',
+    },
+    {
+      img: group,
+      showText:'Grup olarak ziyaret',
+    },
+    {
+      img: official,
+      showText:'Kurumsal eğitim için ziyaret',
+    },
+    {
+      img: sponsor,
+      showText:'Sponsor olmak için ziyaret',
+    },
+   
+  ];
